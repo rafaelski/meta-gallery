84b823ccf298d8a192a1a7fea513c18891874f29
rafaelski <rafaelski@gmail.com>
2022-07-18 10:49:53 -0300

    Add Galeria Template

---
+++ meta_web_Galeria/Assets/InputControls/Scripts/ReloadScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+    
+    public void Reload()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
+}
