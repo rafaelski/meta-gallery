c6136ddcdd98227a4d0e01caa0cd6c01a00a7b92
rafaelski <rafaelski@gmail.com>
2022-07-20 14:26:50 -0300

    .

--- meta_web_Galeria/Assets/Code/Scripts/TeleportToTargets.cs
+++ meta_web_Galeria/Assets/Code/Scripts/TeleportToTargets.cs
@@ -17,39 +17,20 @@ public class TeleportToTargets : MonoBehaviour
     }
 
 
-
     public void Teleport(Transform target)
     {
         // position
         player.transform.position = target.position;
 
-        var temp1 = target.transform.eulerAngles.y;
-        //var temp = GetRotationLikeInspector(target.transform.eulerAngles.y);
-
-        print(temp1);
-        //print(temp);
-
         //rotation
-        //vcam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.Value = GetRotationLikeInspector(target.transform.eulerAngles.y);
-        //vcam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.Value = GetRotationLikeInspector(target.transform.eulerAngles.x);
         vcam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.Value = target.transform.eulerAngles.y;
         vcam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.Value = target.transform.eulerAngles.x;
     }
 
+
     public static float GetRotationLikeInspector(float eulerAngles)
     {
-        //float result = eulerAngles - Mathf.CeilToInt(eulerAngles / 360f) * 360f;
         float result = eulerAngles - 360f;
-
-        //if (eulerAngles < 0)
-        //{
-        //    result = eulerAngles - 360f;
-        //}
-        //else
-        //{
-        //    result = eulerAngles;
-        //}
-
         return result;
     }
 }
